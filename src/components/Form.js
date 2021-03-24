814b36ffa91ff8a3f4454d8a07d8cea8159fd516
Michał Górny <michalgorny94@gmail.com>
2021-03-24 14:32:25 +0100

    Add form and useform, validation func

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import FormSignup from './FormSignup'
+import React, { useState } from 'react';
+import FormSignup from './FormSignup';
+import FormSuccess from './FormSuccess';
 
 
 const Form = () => {
+    const [isSubmitted, setIsSubmitted] = useState(false)
+
+    function submitForm() {
+        setIsSubmitted(true);
+    }
     return (
         <div>
-            <FormSignup></FormSignup>
+            {!isSubmitted ? <FormSignup submitForm=
+            {submitForm} /> : <FormSuccess />}
         </div>
     )
 }
