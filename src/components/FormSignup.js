726c6af8273675c7bfb554f9ba1c06b59ecbaff8
Michał Górny <michalgorny94@gmail.com>
2021-03-24 13:16:56 +0100

    Start react-form project

---
+++ src/components/FormSignup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FormSignup = () => {
+    return (
+        <div className="form-content-right">
+            <form className="form">
+                <h1>Get started woth us today! Create your account by filling out the information below</h1>
+            </form>
+        </div>
+    );
+};
+
+export default FormSignup;
