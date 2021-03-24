72038035e64827f5202a91a672233c9057e52bda
Michał Górny <michalgorny94@gmail.com>
2021-03-24 14:55:27 +0100

    Add style

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
-import FormSignup from './FormSignup';
-import FormSuccess from './FormSuccess';
-
+import React, { useState } from "react";
+import FormSignup from "./FormSignup";
+import FormSuccess from "./FormSuccess";
+import "../style/Form.css";
 
 const Form = () => {
-    const [isSubmitted, setIsSubmitted] = useState(false)
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-    function submitForm() {
-        setIsSubmitted(true);
-    }
-    return (
-        <div>
-            {/* {!isSubmitted ? <FormSignup submitForm=
-            {submitForm} /> : <FormSuccess />} */}
-            <FormSuccess />
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+  return (
+    <>
+      <div className="form-container">
+        <span className="close-btn">x</span>
+        <div className="form-content-left">
+          <img src="img/img-2.svg" alt="spaceship" className="form-img" />
         </div>
-    )
-}
+        {!isSubmitted ? (
+          <FormSignup submitForm={submitForm} />
+        ) : (
+          <FormSuccess />
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Form
+export default Form;
--- src/components/FormSuccess.js
+++ src/components/FormSuccess.js
@@ -4,7 +4,7 @@ const FormSuccess = () => {
   return (
       <div className="form-content-right">
           <div className="form-success">We have received your request!</div>
-          <img src="../img/img-3.svg" alt="success-image" className="form-img-2"/>
+          <img src="img/img-3.svg" alt="success-image" className="form-img-2"/>
       </div>
   );
 };
